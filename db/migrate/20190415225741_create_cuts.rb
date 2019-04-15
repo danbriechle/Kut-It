8265a42bb8b025ee45a4d6643fc97d1b2464ecfc
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 16:58:34 -0600

    Adds users table

---
+++ db/migrate/20190415225741_create_cuts.rb
@@ -0,0 +1,13 @@
+class CreateCuts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cuts do |t|
+      t.string :species
+      t.integer :type
+      t.integer :length
+      t.integer :height
+      t.integer :thickness
+      t.integer :scarf_angle
+      t.references :user, foreign_key: true
+    end
+  end
+end
