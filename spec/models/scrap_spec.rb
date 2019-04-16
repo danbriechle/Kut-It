d1209fd7d68f20205e953cc6bb7f074d416b43c0
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 18:19:22 -0600

    Adds scrap model spec

---
+++ spec/models/scrap_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Scrap do
+  describe 'validations' do
+    it { should validate_presence_of(:species) }
+    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:length) }
+    it { should validate_presence_of(:width) }
+    it { should validate_presence_of(:thickness) }
+  end
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+end
