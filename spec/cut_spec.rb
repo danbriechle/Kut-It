8f2fba204317d703182a9ac004e6e3b0b59ee122
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 18:23:24 -0600

    Adds cut model spec

---
+++ spec/cut_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Cut do
+  describe 'validations' do
+    it { should validate_presence_of(:species) }
+    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:length) }
+    it { should validate_presence_of(:height) }
+    it { should validate_presence_of(:thickness) }
+    it { should validate_presence_of(:scarf_angle) }
+  end
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+end
