86df8540c717781c52d21ca6cef777c52a5b554d
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 20:14:52 -0600

    Adds model test for drum size in cut

--- spec/models/cut_spec.rb
+++ spec/models/cut_spec.rb
@@ -8,6 +8,7 @@ describe Cut do
     it { should validate_presence_of(:height) }
     it { should validate_presence_of(:thickness) }
     it { should validate_presence_of(:scarf_angle) }
+    it { should validate_presence_of(:drumsize) }
   end
   describe 'relationships' do
     it { should belong_to(:user) }
