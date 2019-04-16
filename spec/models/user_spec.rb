ee2b1b2fc4d3b90c334ce1085ddb91b2f6540386
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 18:16:24 -0600

    Fixes test typo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:workplace) }
+    it { should validate_presence_of(:workspace) }
   end
   describe 'relationships' do
     it { should have_many(:scraps) }
