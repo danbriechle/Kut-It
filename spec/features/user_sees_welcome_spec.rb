fa53f6ecbece4f011cdeeecc372261715ee8adf8
Daniel Briechle <danbriechle@hotmail.com>
2019-04-18 15:33:56 -0600

    Appends spec to reflect default user mvp

--- spec/features/user_sees_welcome_spec.rb
+++ spec/features/user_sees_welcome_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 describe 'as a user' do
   context 'when i visit the root' do
     it 'sees welcome message' do
-      user_1 = User.create!(name: "Dan Briechle", workspace: "Briechle Custom Drums")
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
       visit "/"
 
       expect(page).to have_content("Welcome to Kut it!")
       expect(page).to have_link("Demo Our App")
+
+      click_on 'Demo Our App'
+
+      expect(current_path).to eq ('/guest')
     end
   end
 end
