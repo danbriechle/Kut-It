1b9262635dc68368087b5e22ad93aed0fad48e50
Daniel Briechle <danbriechle@hotmail.com>
2019-04-18 15:21:31 -0600

    Adds user sees welcome page spec

---
+++ spec/features/user_sees_welcome_spec.rb
@@ -0,0 +1,14 @@
+describe 'as a user' do
+  context 'when i visit the root' do
+    it 'sees welcome message' do
+      user_1 = User.create!(name: "Dan Briechle", workspace: "Briechle Custom Drums")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit "/"
+
+      expect(page).to have_content("Welcome to Kut it!")
+      expect(page).to have_button("login")
+    end
+  end
+end
