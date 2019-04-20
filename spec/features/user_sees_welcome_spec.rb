e01a38270ee78747acd7f3a0a1702ce2602cdc96
Daniel Briechle <danbriechle@hotmail.com>
2019-04-20 08:50:10 -0600

    Corrects spec

--- spec/features/user_sees_welcome_spec.rb
+++ spec/features/user_sees_welcome_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 describe 'as a user' do
   context 'when i visit the root' do
     it 'sees welcome message' do
+      user_1 = User.create!(name: "Dan Briechle", workspace: "Briechle Custom Drums")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
       visit "/"
 
