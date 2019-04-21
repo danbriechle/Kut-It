eaa32d3fe16f6af9e3cf7b909bc367ea7bbe78ce
Daniel Briechle <danbriechle@hotmail.com>
2019-04-21 09:51:02 -0600

    Adds user workspace to nav bar

--- spec/features/user_sees_nav_spec.rb
+++ spec/features/user_sees_nav_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 describe 'as a user' do
   context 'when i visit my dashboard' do
     it 'sees a nav bar' do
+
       user_1 = User.create!(name: "Dan Briechle", workspace: "Briechle Custom Drums")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-      visit "/woodshop"
+      visit "/"
+
+      click_on 'Demo Our App'
+
+      expect(current_path).to eq ('/woodshop')
 
       within "#right" do
         expect(page).to have_link("Ply Calculator")
         expect(page).to have_link("Scarf Calculator")
+      end
+
+      within "#left" do
+        expect(page).to have_content(user_1.workspace)
       end
     end
   end
