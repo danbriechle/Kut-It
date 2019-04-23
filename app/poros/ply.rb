e3f7d92d91677fb0f03fa94ff90d46bd68db0901
Daniel Briechle <danbriechle@hotmail.com>
2019-04-23 17:26:00 -0600

    Adds slop functionality to ply_length method

--- app/poros/ply.rb
+++ app/poros/ply.rb
@@ -1,17 +1,18 @@
 class Ply
-  attr_reader :species, :thickness, :size, :diameter, :depth, :length, :width
+  attr_reader :species, :thickness, :size, :diameter, :depth, :length, :width ,:slop
   def initialize(ply_params)
     @species = ply_params[:species]
     @thickness = ply_params[:thickness]
     @size = "#{ply_params[:diameter]}X#{ply_params[:depth]}"
-    @length = ply_length(ply_params[:diameter])
+    @length = ply_length(ply_params[:diameter], ply_params[:slop] )
     @width = ply_width(ply_params[:depth])
     @diameter = ply_params[:diameter]
     @depth = ply_params[:depth]
+    @slop = ply_params[:slop]
   end
 
-  def ply_length(diameter)
-    (diameter.to_f * 3.15 + 2).to_i
+  def ply_length(diameter, slop)
+    ((diameter.to_f * 3.15 + 2).to_i * (slop.to_i/100.00 + 1)).round.to_i
   end
 
   def ply_width(depth)
