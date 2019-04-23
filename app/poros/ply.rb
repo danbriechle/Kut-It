9ea300bc95a6d6c6c365a0f5dea240cdf5a76e12
Daniel Briechle <danbriechle@hotmail.com>
2019-04-23 14:29:58 -0600

    Adds correct attributes to ply poro

--- app/poros/ply.rb
+++ app/poros/ply.rb
@@ -1,15 +1,21 @@
 class Ply
-  attr_reader :species, :thickness, :size, :dimensions, :diameter, :depth
+  attr_reader :species, :thickness, :size, :diameter, :depth, :length, :width
   def initialize(ply_params)
     @species = ply_params[:species]
     @thickness = ply_params[:thickness]
     @size = "#{ply_params[:diameter]}X#{ply_params[:depth]}"
-    @dimensions = ply_dimensions(ply_params[:diameter], ply_params[:depth])
+    @length = ply_length(ply_params[:diameter])
+    @width = ply_width(ply_params[:depth])
     @diameter = ply_params[:diameter]
     @depth = ply_params[:depth]
   end
 
-  def ply_dimensions(diameter, depth)
-    "#{(diameter.to_f * 3.15 + 2).to_i}X#{(depth.to_f + 2).to_i}"
+  def ply_length(diameter)
+    (diameter.to_f * 3.15 + 2).to_i
   end
+
+  def ply_width(depth)
+    (depth.to_f + 2).to_i
+  end
+
 end
