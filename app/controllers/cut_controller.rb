ab6b2ceadb45368fa7364f3a828bd0c12c3024c3
Daniel Briechle <danbriechle@hotmail.com>
2019-04-23 15:15:52 -0600

    Adds Cut controller with create method

---
+++ app/controllers/cut_controller.rb
@@ -0,0 +1,19 @@
+class CutController < ApplicationController
+
+  def create
+    cut = current_user.cuts.create!(cut_params)
+    if cut.save
+      redirect_to woodshop_path
+    else
+      redirect_to ply_new_path
+      @error = "Where sorry this could not be added to your cut list"
+    end
+  end
+
+  private
+
+  def cut_params
+    params.permit(:shell_type, :length, :height, :drumsize, :thickness, :scarf_angle, :species)
+  end
+
+end
