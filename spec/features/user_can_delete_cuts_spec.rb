dbe70711a9eb1d8b2d58daf44e3d5e94c148ca2a
Daniel Briechle <danbriechle@hotmail.com>
2019-04-24 10:28:22 -0600

    Adds delete cut spec

---
+++ spec/features/user_can_delete_cuts_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+describe 'as a user' do
+  context 'when i visit the ply calculator' do
+    it 'can calculate ply size' do
+      user_1 = User.create!(name: "Dan Briechle", workspace: "Briechle Custom Drums")
+      scrap_1 = Scrap.create!(species: "Alder", shell_type: 0, length: "46", width: "3", thickness: "3/4", user: user_1)
+      scrap_2 = Scrap.create!(species: "Birch", shell_type: 0, length: "36", width: "8", thickness: "5/8", user: user_1)
+      scrap_3 = Scrap.create!(species: "Cherry", shell_type: 1, length: "67", width: "18", thickness: "1/16", user: user_1)
+      scrap_4 = Scrap.create!(species: "Rosewood", shell_type: 1, length: "41", width: "8", thickness: "1/8", user: user_1)
+      cut_1 = Cut.create!(species: "Rosewood", shell_type: 1, length: "46", height: "8", drumsize: "14x6", thickness: "1/16", scarf_angle: "90", user: user_1)
+      cut_4 = Cut.create!(species: "Mahogany", shell_type: 1, length: "34", height: "6", drumsize: "10x4", thickness: "1/16", scarf_angle: "90", user: user_1)
+      cut_3 = Cut.create!(species: "Wenge", shell_type: 0, length: "48", height: "10", drumsize: "14x8", thickness: "3/4", scarf_angle: "20", user: user_1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit "/"
+
+      click_on 'Demo Our App'
+
+      expect(current_path).to eq('/woodshop')
+      save_and_open_page
+
+      within "#cut-#{cut_1.id}" do
+        expect(page).to have_link("Finished")
+        click_on "Finished"
+      end
+
+      expect(current_path).to eq(woodshop_path)
+
+      expect(page).to have_content("#{cut_1.size} #{cut_1.species} complete!")
+
+      within "#cut_list" do
+        expect(page).to_not have_content(cut_1.drumsize)
+        expect(page).to_not have_content(cut_1.length)
+        expect(page).to_not have_content(cut_1.height)
+      end
+    end
+  end
+end
