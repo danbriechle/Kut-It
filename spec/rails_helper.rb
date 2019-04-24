02167c4e7089ff10f27fe0c0d22045f96c76b5b0
Daniel Briechle <danbriechle@hotmail.com>
2019-04-23 19:55:35 -0600

    Filters un needed files in rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,4 +25,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
-SimpleCov.start "rails"
+SimpleCov.start "rails" do
+  add_filter "app/channels/application_cable/channel.rb"
+  add_filter "app/channels/application_cable/connection.rb"
+  add_filter "app/helpers/application_helper.rb"
+  add_filter "app/jobs/application_job.rb"
+  add_filter "app/mailers/application_mailer.rb"
+  add_filter "app/controllers/application_controller.rb"
+end
