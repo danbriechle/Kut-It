d2a9cdaeb7443577ff0001ecf5a709ca7c3065f9
Daniel Briechle <danbriechle@hotmail.com>
2019-04-23 19:24:52 -0600

    Adds scrap_params method and complete create method

--- app/controllers/scrap_controller.rb
+++ app/controllers/scrap_controller.rb
@@ -1,11 +1,21 @@
 class ScrapController < ApplicationController
+  def new
+    @current_user = current_user
+  end
 
-def new
-  @current_user = current_user
-end
+  def create
+    scrap = current_user.scraps.create!(scrap_params)
+    if scrap.save
+      redirect_to '/woodshop'
+    else
+      render '/new/scrap'
+      @errors = "We're sorry this scrap could not be added to the scrap bin"
+    end
+  end
 
-def create
-  current_user.scraps.create!(scrap_params)
-end
+  private
 
+  def scrap_params
+    params.require(:scrap).permit(:length, :width, :thickness, :species, :shell_type)
+  end
 end
